ff1020271f744fd65ac15622686b70be5330fe16
Богдан Косев <bogdan_gamer@abv.bg>
2022-05-02 14:14:54 +0300

    add content, add flags to select menu

--- javascript/referat.js
+++ javascript/referat.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
-	$(".select-lang").select2();
+	// $(".select-lang").select2();
+
+	$(".select-lang").select2({
+		templateResult: function (item) {
+			return format(item);
+		},
+	});
 
 	$(".select-lang").on("select2:select", function (e) {
 		var data = e.params.data;
@@ -7,3 +13,20 @@ $(document).ready(function () {
 		console.log("Language changed to " + data.text);
 	});
 });
+
+function format(item) {
+	if (!item.id) {
+		return item.text;
+	}
+	var url = "https://hatscripts.github.io/circle-flags/flags/";
+	var img = $("<img>", {
+		class: "img-flag",
+		width: 20,
+		src: url + item.element.id.toLowerCase() + ".svg",
+	});
+	var span = $("<span>", {
+		text: " " + item.text,
+	});
+	span.prepend(img);
+	return span;
+}
